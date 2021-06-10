4f21222e40e03014070ca9f8a39014b73c06a1d2
55XOX55 <2606863445@qq.com>
2021-06-10 17:33:15 +0800

    前端:重点在于html和css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div class="header w">
+            <div class="logo">盒子</div>
+            <div class="nav">
+                <ul>
+                    <li><a href="#">首页</a></li>
+                    <li><a href="#">课程</a></li>
+                    <li><a href="#">职业规划</a></li>
+                </ul>
+            </div>
+            <div class="search">
+                <!--输入框默认值-->
+                <input type="text" value="输入关键词">
+                <button>暗流</button>
+            </div>
+            <div class="user">
+                <!--这个元素是挂载到public下的html中的,路径必须从那里写-->
+                <img src="../src/assets/images/btn1.png" alt="AA">
+                xiaojian
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {}
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import "assets/css/base.css";
 </style>
